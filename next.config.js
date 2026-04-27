853b21152f3fbd19ae09988603b2b6dfb0cbc268
Omar Al-Bakri <164752248+OmarA1-Bakri@users.noreply.github.com>
2026-04-27 15:29:17 +0700

    chore: allow canonical creator avatar hosts

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      { protocol: "https", hostname: "yt3.googleusercontent.com" },
       { protocol: "https", hostname: "yt3.ggpht.com" },
       { protocol: "https", hostname: "i.ytimg.com" },
+      { protocol: "https", hostname: "pbs.twimg.com" },
+      { protocol: "https", hostname: "substackcdn.com" },
+      { protocol: "https", hostname: "cdn.bsky.app" },
+      { protocol: "https", hostname: "i1.sndcdn.com" },
     ],
   },
   experimental: {},
